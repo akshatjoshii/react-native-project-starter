eb547fd221cca9146843a12688de8112fa64746e
Akshat Joshi <akshat2@gmail.com>
2017-09-04 23:30:28 +0530

    travelSafe setup:

---
+++ index.ios.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+
+import App from './src/App';
+
+import { AppRegistry } from 'react-native';
+
+
+
+
+AppRegistry.registerComponent('TravelSafe', () => App);
---
+++ src/App.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Akshat on 01-09-2017.
+ */
+import react, {Component} from 'react';
+import {View} from 'react-native';
+
+export class App extends Component{
+
+    render(){
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
